f119ee623b657fdc2d851a4ba4b242c5706099e6
MartinsDannilo <dannilodev@gmail.com>
2022-04-11 20:27:18 -0300

    arquivos aulas

---
+++ Introdução-ao-Git:Github/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <h1>Meu primeiro site em PHP! Wohooo!</h1>
+</head>
+<body>
+    <?php
+         for ($i = 0 ; $i < 10 ; $i++ ){
+             print ("linha número". $i . "<br />");
+         }
+         ?>
+
+         </body>
+         </html>
